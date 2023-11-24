8be4621826911271e9485710f19e3250857a7748
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-11-24 12:45:11 +0000

    added output scaling

--- source/transform_input_output.py
+++ source/transform_input_output.py
@@ -1,3 +1,4 @@
+import numpy as np
 # Header for standardising inputs and outputs of Gaussian processes
 
 # Function for normalising inputs onto the unit hypercube, using (scalar or 
@@ -14,18 +15,39 @@ def normalise_inputs(x, x_min = [], x_max = [], std = False):
     x_min = x.min(axis = 0)
     x_max = x.max(axis = 0)
   
-  #if (is.matrix(x)){
-  #  N = nrow(x)
-  #} else {
-  #  N = 1
-  #}
   if not std:
-    #x = x - x_min[rep(1,N),]
     x = x - x_min
-  # x_norm = x/(x_max[rep(1,N),]-x_min[rep(1,N),])
   x_norm = x/(x_max-x_min)
  
   if maxmin_out:
     return((x_norm, x_min, x_max))
   else:
     return(x_norm)
+  
+# Function for standardising output using mean mu_y and standard deviation
+# sigma_y to have unit mean and zero standard deviation.
+# std indicates whether y is a standard deviation, in which case it isn't
+# necessary to centre before scaling
+# There is an option to provide the mean vector and standard deviation, 
+# otherwise these are calculated internally
+def standardise_output(y, mu_y = [], sigma_y = [], std = False):
+  if len(mu_y) < 1 or len(sigma_y) < 1:
+    musd_out = True
+  else:
+    musd_out = False
+
+  if not std:
+    if len(mu_y) < 1:
+      mu_y = np.mean(y)
+    
+    y = y - mu_y
+
+  if len(sigma_y) < 1:
+    sigma_y = np.std(y)
+
+  y_scale = y/sigma_y
+  if musd_out:
+    return((y_scale, mu_y, sigma_y))
+  else:
+    return(y_scale)
+  
