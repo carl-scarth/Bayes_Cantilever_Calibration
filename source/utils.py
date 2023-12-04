8a7f26e07868537b4f3e5f877b3dba0795f2163d
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-12-04 16:12:12 +0000

    packaged common plot formatting code

---
+++ source/utils.py
@@ -0,0 +1,14 @@
+# Collection of useful functions used across all examples
+from matplotlib import rcParams
+
+def set_plot_params():
+    # Set commonly used plot parameters to desired values
+    # Plotting parameters
+    rcParams.update({'figure.figsize' : (8,6),
+                    'font.size': 16,
+                    'figure.titlesize' : 18,
+                    'axes.labelsize': 18,
+                    'xtick.labelsize': 15,
+                    'ytick.labelsize': 15,
+                    'legend.fontsize': 15})    
+    
